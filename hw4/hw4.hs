047aecb4b6d21bcd57208f56044311ec3adf1868
jangui <jaimitino@gmail.com>
2020-02-26 21:33:52 -0500

    finished q4

--- hw4/hw4.hs
+++ hw4/hw4.hs
@@ -41,3 +41,37 @@ mapTree func (Node val left right) = Node (func val) (mapTree func left) (mapTre
 mapTree _ Leaf = Leaf
 
 --Question 4
+
+bstInsert :: Ord a => Tree (a, Int) -> a -> Tree(a, Int)
+bstInsert Leaf val = (Node (val, 1) Leaf Leaf)
+bstInsert (Node (v, n) l r) val = 
+--check if val same as parent node
+  if val == v
+    --if so inc count
+    then (Node (v, n+1) l r)
+    else 
+        --check if greater than
+        if val > v
+          then 
+          --we have to insert into right
+            case r of
+              --if right spot open, insert
+              Leaf -> (Node (v, n) l (Node (val, 1) Leaf Leaf))
+              --if not open, check if we should increment the count on right, or insert
+              (Node (v2, n2) l2 r2) ->
+                if val == v 
+                  then
+                    (Node (v, n) l (Node (v2, n2+1) l2 r2))
+                  else (Node (v, n) l (bstInsert (Node (v2, n2) l2 r2) val))
+          else 
+            --val is less, insert to left
+            case l of
+              --if space, pop it in
+              Leaf -> (Node (v, n) (Node (val, 1) Leaf Leaf) r)
+              -- else check if left node needs an increment, or an insert
+              (Node (v2, n2) l2 r2) ->
+                if val == v
+                  then (Node (v, n) (Node (v2, n2+1) l2 r2) r)
+                  else (Node (v,n) (bstInsert (Node (v2, n2) l2 r2) val) r)
+
+--Question 5
