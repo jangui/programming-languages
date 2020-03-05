8fdacad10c1b112a2b7dfa415f0a4796917164b7
jangui <jaimitino@gmail.com>
2020-03-04 22:12:08 -0500

    finishing up hw5

--- hw5/hw5.hs
+++ hw5/hw5.hs
@@ -24,3 +24,51 @@ streamToList (Chunk v next) = v : streamToList next
 streamRepeat :: a -> Stream a
 streamRepeat a = Chunk a (streamRepeat a)
 
+--Question 3
+instance Functor Stream where
+  fmap f (Chunk a s) = Chunk (f a) (fmap f s)
+
+--Question 4
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f a =
+  let v = f a
+  in Chunk v (streamFromSeed f v)
+
+--Question 5
+data Tree a = Node a (Tree a) (Tree a)
+            | Leaf
+
+bigTree :: Tree Integer
+bigTree = bigTreeHelp (Node 0 Leaf Leaf)
+
+bigTreeHelp :: Tree Integer -> Tree Integer
+bigTreeHelp (Node v Leaf Leaf) = 
+  let t = bigTreeHelp (Node (v+1) Leaf Leaf)
+  in Node v t t
+
+--Question 6
+collatz :: Integer -> Stream Integer
+collatz i =
+  if ((even i) == True)
+    then Chunk i (collatz (div i 2))
+    else Chunk i (collatz (3*i+1))
+
+--Question 7
+collatzLength :: Integer -> Integer
+collatzLength n = 
+  case collatz n of
+    (Chunk 1 s) -> 0
+    (Chunk a (Chunk b s)) -> 1 + collatzLength b
+
+--Question 8
+longestCollatz :: Integer -> Integer
+longestCollatz i = v
+  where (maxlen, v) = largest (zip (map collatzLength [1..i]) [1..i])
+
+largest :: [(Integer, Integer)] -> (Integer, Integer)
+largest [a] = a
+largest ((l,v):t) =
+  if l > maxlen
+    then (l,v)
+    else (maxlen, v2)
+    where (maxlen, v2) = largest t
