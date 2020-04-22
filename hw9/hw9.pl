8e2add57a32a58ce9d2429d17e375ab1edb5a4d4
jangui <jaimitino@gmail.com>
2020-04-21 22:54:33 -0400

    finished hw9

--- hw9/hw9.pl
+++ hw9/hw9.pl
@@ -27,3 +27,41 @@ f) false
 g) false
 
 */
+
+% Question 8
+inboth([X|_], [X|_], X).
+inboth([X|T], [_|T2], X) :- inboth([X|T], T2, X).
+inboth([_|T], [X|T2], X) :- inboth(T, [X|T2], X).
+inboth([_|T], [_|T2], X) :- inboth(T, T2, X).
+
+% Question 9
+increment_all([], []).
+increment_all([H|T], [I|T2]) :- I is H+1, increment_all(T, T2).
+
+% Question 10
+foldup(X, X).
+foldup([H, H2 | T], L) :- S is H + H2, foldup([S|T], L).
+
+/* Question 11
+T1 = [[a, f(b), c, d], g(X1, [e|X2]), X3, X4|X5]
+T2 = [Y1, g(d, Y2) | Y1]
+
+Y1 needs to unify with [a, f(b), c, d].
+Y1 = [a, f(b), c, d]
+
+g(X1, [e|X2]) needs to unify with g(d, Y2)
+so X1 needs to unify with d and [e|X2] needs to unify with Y2
+X1 = d
+Y2 = [e|X2]
+
+[X3, X4 | X5] needs to unify with Y1. Therefore X3 = a, X4 = f(b), X5 = [c,d] 
+
+No constraints we're met in the unification so T1 does unify to T2 with the specified values.
+*/
+
+% Question 12
+all_diff([]).
+all_diff([H|T]) :- all_diff(T), not(member(H, T)).
+
+% Question 13
+splitAt(Pos, List, A, B) :- append(A, B, List), length(A, Pos).
